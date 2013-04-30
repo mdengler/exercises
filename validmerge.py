8b6daa330bbcc0658e9fb33d6269e13aa5f66915
Martin Dengler <martin@martindengler.com>
2013-04-30 20:59:42 +0100

    validmerge: initial commit

---
+++ validmerge.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+def validmerge(m, x, y):
+    if ((m == x and len(y) < 1)
+        or
+        (m == y and len(x) < 1)):
+        return True
+
+    elif len(m) < 1 or len(y) < 1 or len(x) < 1:
+        return False
+
+    elif all(p == m[0] for p in (x[0], y[0])):
+        return (validmerge(m[1:], x[1:], y)
+                or
+                validmerge(m[1:], x, y[1:]))
+
+    elif m[0] == x[0]:
+        return validmerge(m[1:], x[1:], y)
+
+    elif m[0] == y[0]:
+        return validmerge(m[1:], x, y[1:])
+
+    else:
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
