07530c662f2c938be22fd7c1e9c4b1792e99304f
Martin Dengler <martin@martindengler.com>
2013-05-01 01:14:00 +0100

    validmerge.py: add tests and more general merge case

--- validmerge.py
+++ validmerge.py
@@ -25,8 +25,86 @@ def validmerge(m, x, y):
         return False
 
 
+def validmerge2(m, inputs):
+    if m in inputs and not any(i for i in inputs if i != m):
+        return True
+
+    elif len(m) < 1 or not any(inputs):
+        return False
+
+    else:
+      if m[0] in [i[0:1] for i in inputs]:
+        possibilities = [p for p in inputs if p[0:1] == m[0]]
+        for possibility in possibilities:
+            other_inputs = inputs[:]
+            other_inputs.remove(possibility)
+            if validmerge2(m[1:], [possibility[1:]] + other_inputs):
+                return True
+
+    return False
+
+
+
+def __test__():
+    TESTS = [
+        [["", ["", ""]], True],
+        [["A", ["A", ""]], True],
+        [["B", ["", "B"]], True],
+        [["AA", ["AA", ""]], True],
+        [["BB", ["", "BB"]], True],
+        [["ABA", ["A", "BA"]], True],
+        [["BB", ["", "BB"]], True],
+        [["BB", ["B", "B"]], True],
+
+        [["", ["F", ""]], False],
+        [["", ["", "F"]], False],
+        [["F", ["", ""]], False],
+        [["FF", ["", "F"]], False],
+        [["F", ["A", "B"]], False],
+
+        [["EFGABCDEF", ["EGBDF", "FACE"]], True],
+        [["DEADBEEF", ["DEAD", "BEEF"]], True],
+        [["314159265", ["34525", "1196"]], True],
+
+        ]
+
+    passed = False
+    for test_args, expected_result in TESTS:
+        v1 = None
+        v2 = None
+        try:
+            v1 = validmerge(test_args[0], *test_args[1])
+        except Exception, msg:
+            v1 = "failed: %s" % msg
+        try:
+            v2 = validmerge2(*test_args)
+        except Exception, msg:
+            v2 = "failed: %s" % msg
+
+        if v1 != v2:
+            print "FAIL: expected %s, but INCONSISTENT: %s vs %s" % (expected_result, v1, v2)
+        elif v1 != expected_result:
+            print "FAIL: expected %s, but got: %s / %s" % (expected_result, v1, v2)
+        else:
+            assert v1 == v2 == expected_result
+            print "GOOD: '%s' %s a valid merge of %s" % (test_args[0], "is" if v1 else "is not", test_args[1])
+            passed = True
+
+    return passed
+
+
+
 if __name__ == "__main__":
-    sys.exit(0 if validmerge(*sys.argv[1:]) else 1)
+    args = sys.argv[1:]
+
+    retval = 1
+    if len(args) == 3:
+        if validmerge(*args):
+            retval = 0
+    elif __test__():
+        retval = 0
+
+    sys.exit(retval)
 
 
 
